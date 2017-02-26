51c89bc9c6becd8413f7bb44218a480a4f55c7b6
fanjianhai <18210836561@163.com>
2017-02-26 17:16:24 +0800

    初始化仓库

---
+++ Safe-Java-JS-WebView-Bridge-Demo/app/src/main/java/com/xiaofan/safe_java_js_webview_bridge_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xiaofan.safe_java_js_webview_bridge_demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Safe-Java-JS-WebView-Bridge-Demo/app/src/test/java/com/xiaofan/safe_java_js_webview_bridge_demo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.xiaofan.safe_java_js_webview_bridge_demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
